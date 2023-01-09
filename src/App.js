78b510800a42a39a9c9053c469aec54cac8d8b01
ytkuo <hahaeating1@gmail.com>
2023-01-09 00:07:24 -0500

    1/8 front-end v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./style/style.css";
+import Video from "./components/Video";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [status, setStatus] = useState('')
+	const [data, setData] = useState('')
+
+	async function callAPI(url) {
+		setStatus('Downloading...')
+		const responseDownload = await fetch(
+			'/download',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: JSON.stringify(url)
+			}
+		);
+		const data = await responseDownload.json(); // Extracting data as a JSON Object from the response
+
+		setStatus('Spleeting...')
+		const responseSpleeter = await fetch(
+			'/spleet',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+					// 'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				body: JSON.stringify(data)
+			}
+		);
+		const result = await responseSpleeter.json(); // Extracting data as a JSON Object from the response
+
+		setStatus('Success...')
+		setData(result.filename)
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		callAPI(e.target.url.value)
+		// setStatus('Downloading...')
+		// fetch("/download",{
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 		// 'Content-Type': 'application/x-www-form-urlencoded',
+		// 	},
+		// 	body: JSON.stringify(e.target.url.value)
+		// })
+		// .then(res => {
+		// 	setStatus('Spleeting...')
+		// 	res.json()
+		// })
+		// .then(data => {
+		// 	fetch("/spleet",{
+		// 		method: 'POST',
+		// 		headers: {
+		// 			'Content-Type': 'application/json'
+		// 		},
+		// 		body: JSON.stringify(data)
+		// 	})
+		// })
+		// .then(res => res.json())
+		// .then(data => {
+		// 	setStatus('Success!')
+		// 	setData(data.filename)
+		// 	console.log(data)
+		// })
+	}
+
+	return (
+		<div>
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="url">URL:</label>
+					<input type="text" id="url" name="url" />
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+			{/* <div>
+				<Video />
+			</div> */}
+			<p>{status}</p>
+			{typeof data !== 'undefined' && <p>{data}</p> }
+		</div>
+	)
 }
 
 export default App;
