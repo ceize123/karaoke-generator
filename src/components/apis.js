ffeaaf5cd2aeb428b3077c510245e356a92b45df
ytkuo <hahaeating1@gmail.com>
2023-01-23 19:55:32 -0500

    1/23 add env file

--- src/components/apis.js
+++ src/components/apis.js
@@ -1,7 +1,11 @@
+// https://stackoverflow.com/questions/42458434/how-to-set-build-env-variables-when-running-create-react-app-build-script
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : ''
+console.log(apiUrl)
+
 // Call api search
 export async function search(url) {
 	const responseSearch = await fetch(
-		'/search',
+		`${apiUrl}/search`,
 		{
 			method: 'POST',
 			headers: {
@@ -16,7 +20,7 @@ export async function search(url) {
 // Call api download
 export async function download(url) {
 	const responseDownload = await fetch(
-		'/download',
+		`${apiUrl}/download`,
 		{
 			method: 'POST',
 			headers: {
@@ -32,7 +36,7 @@ export async function download(url) {
 export async function spleeter(res) {
 	// https://stackoverflow.com/questions/68230294/how-can-i-play-audio-file-sent-from-flask-send-file
 	const responseSpleeter = await fetch(
-		'/spleet',
+		`${apiUrl}/spleet`,
 		{
 			method: 'POST',
 			headers: {
