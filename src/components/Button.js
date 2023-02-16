320e9c4ad7d57dd58b3bcd670357336a9252d7e8
ytkuo <hahaeating1@gmail.com>
2023-02-16 17:22:08 -0500

    indentation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,17 @@
-export default function Button({content, onHandleChange}) {
+export default function Button({ content, onHandleChange }) {
 	const handleChangeStateClick = (content) => {
 		const res = content === 'true' ? true : content
-		onHandleChange(res);
-	};
+		onHandleChange(res)
+	}
 	return (
 		<button
-			onClick={() => { handleChangeStateClick(content) }}
+			onClick={() => {
+				handleChangeStateClick(content)
+			}}
 			type='button'
 			className='inline-flex items-center rounded-md border border-transparent bg-indigo-100 px-4 py-2 text-sm font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-		>{content === 'true' ? 'Convert' : 'Choose'}</button>
+		>
+			{content === 'true' ? 'Convert' : 'Choose'}
+		</button>
 	)
 }
