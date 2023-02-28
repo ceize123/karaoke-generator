a4edcff17731b3b7bee546f9a9a024a27d4eafbb
ytkuo <hahaeating1@gmail.com>
2023-02-27 22:43:01 -0500

    search bar ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,11 @@ import '../style/style.css'
 import MusicDataService from '../services/music.service'
 import { SearchSec, SearchSecSingle } from '../components/Search-Sec'
 import { v4 as uuidv4 } from 'uuid'
+import pattern from '../img/bg-pattern.png'
+import orbit from '../img/orbit.png'
+import bgBar from '../img/bg-input-bar.png'
 
-function isValidHttpUrl(string) {
+function isValidHttpURL(string) {
   let url
   try {
     url = new URL(string)
@@ -38,16 +41,6 @@ function Home() {
     } catch {
       setError(true)
     }
-
-    // MusicDataService.download(data)
-    // 	.then((res) => {
-    // 		console.log(res.data)
-    // 	}).then(() => {
-    // 		addToTask()
-    // 	}).catch((e) => {
-    // 		console.log(e)
-    // 		setError(true)
-    // 	})
   }
 
   const addToTask = async () => {
@@ -66,24 +59,17 @@ function Home() {
     } catch {
       setError(true)
     }
-    // MusicDataService.generate(data)
-    // 	.then((res) => {
-    // 		setData(URL.createObjectURL(res.data))
-    // 		setStatus('Success!')
-    // 		setActive(false)
-    // 	}).catch((e) => {
-    // 		console.log(e)
-    // 		setError(true)
-    // 	})
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     setError(false)
-    const inputVal = e.target.val.value
+
+    const keyword = e.target.music.value
+    console.log(keyword)
     const data = {
-      val: inputVal,
-      isUrl: isValidHttpUrl(inputVal),
+      val: keyword,
+      isUrl: isValidHttpURL(keyword),
     }
 
     try {
@@ -92,14 +78,6 @@ function Home() {
     } catch {
       setError(true)
     }
-    // MusicDataService.search(data)
-    // 	.then((res) => {
-    // 		console.log(res.data.videos)
-    // 		setSearchRes(res.data.videos)
-    // 	}).catch((e) =>{
-    // 		console.log(e)
-    // 		setError(true)
-    // 	})
   }
 
   const onHandleChange = (res) => {
@@ -119,11 +97,6 @@ function Home() {
       sessionStorage.setItem('uid', JSON.stringify(uid))
     }
     setActive(false)
-    // const intervalId = setInterval(() => {
-    //   const obj = JSON.parse(sessionStorage.uid)
-    //   console.log(obj)
-    // }, 5000)
-    // return () => clearInterval(intervalId)
   }, [])
 
   window.addEventListener('beforeunload', () => {
@@ -136,33 +109,50 @@ function Home() {
 
   return (
     <div className='container max-w-7xl mx-auto'>
-      <main className=' flex items-center flex-col'>
-        <section className='mt-10 max-w-5xl w-1/2'>
-          <form onSubmit={handleSubmit} className='text-center'>
+      <div className='absolute top-0 right-0 w-[45vw] max-w-4xl'>
+        <div className='relative flex justify-end'>
+          <div className='w-4/5'>
+            <img className='w-full h-auto' src={pattern} alt='pattern' />
+          </div>
+          <div className='triangle text-pink absolute top-0 left-0'></div>
+          <div className='triangle triangle2 text-pink absolute top-[20vh] left-[5vw] 3xl:hidden'></div>
+          <img
+            className='w-[7vw] max-w-[200px] orbit absolute bottom-2 left-[15vw] 2xl:left-56'
+            src={orbit}
+            alt='orbit'
+          />
+        </div>
+      </div>
+      <main>
+        <section className='w-full h-screen relative grid sm:grid-cols-12 2xl:grid-cols-1 items-center'>
+          <form
+            onSubmit={handleSubmit}
+            className='text-center sm:col-span-8 2xl:col-span-1 sm:col-start-2 mx-3 sm:mx-0'
+          >
             <div>
-              <label htmlFor='val' className='block text-5xl mb-3'>
-                YouTube Video Converter
-              </label>
-              <input
-                className='block border-2 p-2 w-full rounded-md mb-3'
-                type='text'
-                id='val'
-                name='val'
-                placeholder='Paste Your Link Here'
-              />
+              <h1 className='mb-2'>Karaoke Generator</h1>
+              <p className='lg:mb-4 mb-2'>Easily remove vocals from music</p>
+              <div
+                className='w-full flex justify-center items-center sm:h-[6vw] max-h-[75px] h-12'
+                style={{
+                  background: `url(${bgBar}) no-repeat center center / contain`,
+                }}
+              >
+                <input
+                  className='border-4 border-primary py-1 px-4 2xl:w-3/5 sm:w-[93%] w-4/5 rounded-3xl'
+                  type='text'
+                  id='music'
+                  name='music'
+                  placeholder='Youtube link, song, any key words'
+                />
+              </div>
             </div>
-            <button
-              className='inline-flex items-center rounded-md border border-transparent 
-                            bg-gray-600 px-4 py-2 text-base font-medium text-white shadow-sm
-                            hover:bg-gray-700 focus:outline-none focus:ring-2
-                            focus:ring-gray-500 focus:ring-offset-2'
-              type='submit'
-            >
+            <button className='bg-primary' type='submit'>
               Search
             </button>
           </form>
         </section>
-        {searchRes && (
+        {searchRes.length > 0 && (
           <section className='w-full flex justify-center my-8'>
             {searchRes.length === 1 ? (
               <SearchSecSingle
